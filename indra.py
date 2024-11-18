91fd6663c77f35b9093b5be4a7beecf6780685d6
khorchf <elmadisamir2002@gmail.com>
2024-11-18 16:28:16 +0000

    Add files via upload

---
+++ indra.py
@@ -0,0 +1,57 @@
+# indra =v0.0.1
+
+
+def _banner():
+    print("""
+
+        
+
+            ██╗███╗   ██╗██████╗ ██████╗  █████╗     
+            ██║████╗  ██║██╔══██╗██╔══██╗██╔══██╗    
+            ██║██╔██╗ ██║██║  ██║██████╔╝███████║    
+            ██║██║╚██╗██║██║  ██║██╔══██╗██╔══██║    
+            ██║██║ ╚████║██████╔╝██║  ██║██║  ██║    
+            ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    
+                                                         
+            
+            ➥  dev by ❝ khorchf ❞ , github @khorchf  
+            ➥  version v0.0.1                      
+            ➥  work on all unix/linux distribuion's 
+            ➥                                                                           
+                                                                                
+    """)
+
+_banner()
+
+
+target = input(" ➤ ")
+
+def _domain():
+    print(target)
+    if target == "www.akwagroup.com":
+        print(" - Target : www.akwagroup.com => port open '8080' ")
+    else:
+        print(" an error ACCURED! ")
+
+_domain()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
